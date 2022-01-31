165ed076a1ffc83e35606d800d813ded0d7f0357
Ren Wang <iamwrm@gmail.com>
2022-01-31 12:31:27 +0000

    fixing dependency in ci/deb_install.sh

---
+++ ci/deb_install.sh
@@ -0,0 +1,8 @@
+export DEBIAN_FRONTEND=noninteractive
+apt update -y && apt upgrade -y
+apt install -y \
+    clang-13 lld-13 \
+    curl \
+    make
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
